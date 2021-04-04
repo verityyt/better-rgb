3fc120b030e6adfa1d9f5d57bcf76030107a185f
Verity <joshua-schmidt@online.de>
2021-04-04 16:46:47 +0200

    :construction: Add main class

---
+++ src/main/java/BetterRGB.kt
@@ -0,0 +1,15 @@
+import userinterface.WindowHandler
+import utils.Logger
+
+object BetterRGB {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        Logger.info("Starting up BetterRGB...")
+
+        WindowHandler.open()
+
+    }
+
+}
