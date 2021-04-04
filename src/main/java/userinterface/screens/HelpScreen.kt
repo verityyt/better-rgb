6cf6d441a7c581fc498c6b047bb44d89756f4692
Verity <joshua-schmidt@online.de>
2021-04-04 22:39:52 +0200

    :sparkles: Finish HelpScreen (for now)

--- src/main/java/userinterface/screens/HelpScreen.kt
+++ src/main/java/userinterface/screens/HelpScreen.kt
@@ -1,6 +1,7 @@
 package userinterface.screens
 
 import userinterface.ColorPalette
+import userinterface.CustomFont
 import userinterface.Screen
 import java.awt.Graphics
 import java.awt.Graphics2D
@@ -9,9 +10,33 @@ import java.awt.image.ImageObserver
 class HelpScreen : Screen() {
 
     override fun paint(g: Graphics, g2: Graphics2D, observer: ImageObserver) {
+
+        /* Software Information */
+
         g.color = ColorPalette.foreground
-        g.font = g.font.deriveFont(42f)
-        g.drawString("Hello there!", 100, 100)
+        g.font = CustomFont.bold?.deriveFont(22f)
+        g.drawString("Software Information:", 180, 70)
+
+        g.font = CustomFont.regular?.deriveFont(18f)
+        g.drawString("Version:", 205, 108)
+        g.drawString("v${BetterRGB.version}", 375, 108)
+
+        g.drawString("Build date:", 205, 135)
+        g.drawString(BetterRGB.buildDate, 375, 135)
+
+        /* Credits */
+
+        g.color = ColorPalette.foreground
+        g.font = CustomFont.bold?.deriveFont(22f)
+        g.drawString("Credits:", 745, 70)
+
+        g.font = CustomFont.regular?.deriveFont(18f)
+        g.drawString("Designer:", 770, 108)
+        g.drawString("verity", 940, 108)
+
+        g.drawString("Head Developer:", 770, 135)
+        g.drawString("verity", 940, 135)
+
     }
 
     override fun mouseClicked(x: Int, y: Int) {}
