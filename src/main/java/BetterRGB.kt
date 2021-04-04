7abc5206015f051db86893ad809ecf65029cf49e
Verity <joshua-schmidt@online.de>
2021-04-04 22:30:28 +0200

    :construction: Add build date and software version to main class

--- src/main/java/BetterRGB.kt
+++ src/main/java/BetterRGB.kt
@@ -1,13 +1,20 @@
+import userinterface.CustomFont
 import userinterface.WindowHandler
 import utils.Logger
+import java.text.SimpleDateFormat
+import java.util.*
 
 object BetterRGB {
+
+    var version = "0.1.2"
+    var buildDate = SimpleDateFormat("MM/dd/YYYY").format(Date())
 
     @JvmStatic
     fun main(args: Array<String>) {
 
         Logger.info("Starting up BetterRGB...")
 
+        CustomFont.registerFonts()
         WindowHandler.openWindow()
 
     }
