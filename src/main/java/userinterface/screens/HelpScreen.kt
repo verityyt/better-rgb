f66bddd3046428061dea53e1592f09657213c0c9
Verity <joshua-schmidt@online.de>
2021-04-04 22:44:57 +0200

    :construction: Update HelpScreen

--- src/main/java/userinterface/screens/HelpScreen.kt
+++ src/main/java/userinterface/screens/HelpScreen.kt
@@ -21,8 +21,11 @@ class HelpScreen : Screen() {
         g.drawString("Version:", 205, 108)
         g.drawString("v${BetterRGB.version}", 375, 108)
 
-        g.drawString("Build date:", 205, 135)
-        g.drawString(BetterRGB.buildDate, 375, 135)
+        g.drawString("Build date:", 205, 155)
+        g.drawString(BetterRGB.buildDate, 375, 155)
+
+        g.drawString("Operating System:", 205, 205)
+        g.drawString(System.getProperty("os.name"), 375, 205)
 
         /* Credits */
 
@@ -31,11 +34,11 @@ class HelpScreen : Screen() {
         g.drawString("Credits:", 745, 70)
 
         g.font = CustomFont.regular?.deriveFont(18f)
-        g.drawString("Designer:", 770, 108)
+        g.drawString("Design:", 770, 108)
         g.drawString("verity", 940, 108)
 
-        g.drawString("Head Developer:", 770, 135)
-        g.drawString("verity", 940, 135)
+        g.drawString("Head Developer:", 770, 155)
+        g.drawString("verity", 940, 155)
 
     }
 
