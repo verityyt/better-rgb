5d00d6f42360cb4a3c767d154029dc62823666d9
Verity <joshua-schmidt@online.de>
2021-04-04 18:18:28 +0200

    :sparkles: Add HelpScreen (empty)

---
+++ src/main/java/userinterface/screens/HelpScreen.kt
@@ -0,0 +1,22 @@
+package userinterface.screens
+
+import userinterface.Screen
+import java.awt.Graphics
+import java.awt.Graphics2D
+import java.awt.image.ImageObserver
+
+class HelpScreen : Screen() {
+
+    override fun paint(g: Graphics, g2: Graphics2D, observer: ImageObserver) {
+
+    }
+
+    override fun mouseClicked(x: Int, y: Int) {
+
+    }
+
+    override fun mouseMoved(x: Int, y: Int) {
+
+    }
+
+}
