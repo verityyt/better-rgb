42a5bde3fa041c131ba460b1cfec57b0c7f03c81
Verity <joshua-schmidt@online.de>
2021-04-04 16:49:21 +0200

    :sparkles: Add extention function for Graphics2D opacity

---
+++ src/main/java/Extentions.kt
@@ -0,0 +1,10 @@
+import java.awt.AlphaComposite
+import java.awt.Graphics2D
+
+fun Graphics2D.setOpacity(opacity: Float) {
+    this.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity)
+}
+
+fun Graphics2D.resetOpacity() {
+    this.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f)
+}
