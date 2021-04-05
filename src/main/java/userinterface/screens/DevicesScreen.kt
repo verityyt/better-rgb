327a9a9038673e49f92c39db8ef605bb0c6ad949
Verity <joshua-schmidt@online.de>
2021-04-05 09:51:52 +0200

    :sparkles: Implement devices screen with try again button if openrgb isn't connected yet

--- src/main/java/userinterface/screens/DevicesScreen.kt
+++ src/main/java/userinterface/screens/DevicesScreen.kt
@@ -1,6 +1,10 @@
 package userinterface.screens
 
+import openrgb.OpenRGBManager
+import resetOpacity
+import setOpacity
 import userinterface.ColorPalette
+import userinterface.CustomFont
 import userinterface.Screen
 import java.awt.Graphics
 import java.awt.Graphics2D
@@ -8,14 +12,49 @@ import java.awt.image.ImageObserver
 
 class DevicesScreen : Screen() {
 
+    private var buttonHovered = false
+
     override fun paint(g: Graphics, g2: Graphics2D, observer: ImageObserver) {
-        g.color = ColorPalette.foreground
-        g.font = g.font.deriveFont(42f)
-        g.drawString("General Kenobi", 100, 100)
+
+        if (OpenRGBManager.connected) {
+
+        } else {
+
+            g.color = ColorPalette.foreground
+            g.font = CustomFont.regular?.deriveFont(36f)
+            g.drawString("Could not connect to OpenRGB", 375, 305)
+
+            g.font = CustomFont.regular?.deriveFont(18f)
+            g.drawString("Please start SDK Server in OpenRGB, and try again.", 425, 340)
+
+            /* Draw 'try again' button */
+
+            g2.color = ColorPalette.foreground
+            g2.setOpacity(if(buttonHovered) {
+                0.4f
+            }else {
+                0.2f
+            })
+            g2.fillRect(550, 370, 140, 35)
+            g2.resetOpacity()
+
+            g.font = CustomFont.regular?.deriveFont(18f)
+            g.drawString("TRY AGAIN", 576, 395)
+
+        }
+
     }
 
-    override fun mouseClicked(x: Int, y: Int) {}
+    override fun mouseClicked(x: Int, y: Int) {
+        if(x in 549..689 && y in 349..404) {
+            Thread {
+                OpenRGBManager.connect()
+            }.start()
+        }
+    }
 
-    override fun mouseMoved(x: Int, y: Int) {}
+    override fun mouseMoved(x: Int, y: Int) {
+        buttonHovered = (x in 549..689 && y in 349..404)
+    }
 
 }
