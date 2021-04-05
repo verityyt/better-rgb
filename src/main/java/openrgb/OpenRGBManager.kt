2a53ebab392253af71bb14250b4d70d2c22ee7e7
Verity <joshua-schmidt@online.de>
2021-04-05 09:51:07 +0200

    :bug: Fix 'connect' and 'updateDevices' function of OpenRGBManager

--- src/main/java/openrgb/OpenRGBManager.kt
+++ src/main/java/openrgb/OpenRGBManager.kt
@@ -13,12 +13,13 @@ object OpenRGBManager {
     var devices = HashMap<String, Int>()
 
     fun connect() {
-        Logger.debug("Connecting to OpenRGB...")
+        Logger.debug("Trying to connect to OpenRGB...")
 
         try {
             client = OpenRGBClient("localhost", 6742, "BetterRGB | v${BetterRGB.version} - ${BetterRGB.buildDate}")
 
             client.connect()
+            connected = true
 
             Logger.info("Successfully connected to OpenRGB!")
         } catch (e: ConnectException) {
@@ -32,23 +33,27 @@ object OpenRGBManager {
     fun updateDevices() {
         Logger.debug("Updating OpenRGB devices...")
 
-        for(index in 0 until client.controllerCount) {
-            val device = client.getDeviceController(index)
-            var name = device.name
-            name = name.substring(0, name.length - 1)
+        if(connected) {
+            for(index in 0 until client.controllerCount) {
+                val device = client.getDeviceController(index)
+                var name = device.name
+                name = name.substring(0, name.length - 1)
 
-            while (true) {
-                if(name.endsWith(" ")) {
-                    name = name.substring(0, name.length - 1)
-                }else {
-                    break
+                while (true) {
+                    if(name.endsWith(" ")) {
+                        name = name.substring(0, name.length - 1)
+                    }else {
+                        break
+                    }
                 }
+
+                devices[name] = index
             }
 
-            devices[name] = index
+            Logger.info("Successfully updated OpenRGB devices!")
+        }else {
+            Logger.warn("Skipped device updating, because client is not connected yet")
         }
-
-        Logger.info("Successfully updated OpenRGB devices!")
     }
 
     fun getDeviceZoneNames(deviceIndex: Int): HashMap<String, Int> {
