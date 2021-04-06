c8c6c87869367eeb547e0a94e1c90f098c460ab2
Verity <joshua-schmidt@online.de>
2021-04-06 18:09:05 +0200

    :boom: Implement 'Effect' button + EffectPopup for every zone

---
+++ src/main/java/userinterface/popups/EffectPickerPopup.kt
@@ -0,0 +1,235 @@
+package userinterface.popups
+
+import openrgb.EffectsEnum
+import resetOpacity
+import setOpacity
+import userinterface.ColorPalette
+import userinterface.CustomFont
+import userinterface.Popup
+import userinterface.widgets.ColorSliderWidget
+import java.awt.Color
+import java.awt.Graphics
+import java.awt.Graphics2D
+import java.awt.image.ImageObserver
+
+class EffectPickerPopup(
+    val exec: (red: Int, green: Int, blue: Int) -> Unit,
+    private val oldRed: Int = 0,
+    private val oldGreen: Int = 0,
+    private val oldBlue: Int = 0
+) : Popup() {
+
+    private var redSlider = ColorSliderWidget(385, 275, Color.red, "R", oldRed)
+    private var greenSlider = ColorSliderWidget(385, 315, Color.green, "G", oldGreen)
+    private var blueSlider = ColorSliderWidget(385, 355, Color.blue, "B", oldBlue)
+
+    private var widgets = listOf(redSlider, greenSlider, blueSlider)
+
+    private var hoveredApply = false
+    private var hoveredCancel = false
+    override var open = true
+
+    var needSecondaryColor = false
+
+    private var effect: EffectsEnum = EffectsEnum.STATIC
+
+    override fun paint(g: Graphics, g2: Graphics2D, observer: ImageObserver) {
+
+        if (open) {
+            g.color = Color.decode("#555555")
+            g.fillRoundRect(250 - 50, 220 + 30, 850, 200, 25, 25)
+
+            if (needSecondaryColor) {
+                g2.resetOpacity()
+
+                for (widget in widgets) {
+                    widget.paint(g, g2, observer)
+                    widget.available = true
+                }
+
+                /* Preview */
+
+                g.color = ColorPalette.foreground
+                g.fillRoundRect(220, 270, 110, 110, 25, 25)
+
+                g.color = Color(redSlider.value, greenSlider.value, blueSlider.value)
+                g.fillRoundRect(225, 275, 100, 100, 25, 25)
+
+            } else {
+
+                for (widget in widgets) {
+                    widget.paint(g, g2, observer)
+                    widget.available = false
+                }
+
+                /* Preview */
+
+                g2.setOpacity(0.4f)
+                g2.color = ColorPalette.foreground
+                g2.fillRoundRect(220, 270, 110, 110, 25, 25)
+
+                g2.color = Color(redSlider.value, greenSlider.value, blueSlider.value)
+                g2.fillRoundRect(225, 275, 100, 100, 25, 25)
+
+            }
+
+            /* Done button */
+
+            g2.color = ColorPalette.foreground
+            g2.setOpacity(
+                if (hoveredApply) {
+                    1f
+                } else {
+                    0.6f
+                }
+            )
+
+            g2.font = CustomFont.light?.deriveFont(24f)
+            g2.drawString("Apply", 980, 280)
+
+            g2.color = ColorPalette.foreground
+            g2.setOpacity(
+                if (hoveredCancel) {
+                    1f
+                } else {
+                    0.6f
+                }
+            )
+            g2.drawString("Cancel", 970, 430)
+
+            /* Effects */
+
+            when (effect) {
+                EffectsEnum.STATIC -> {
+                    g2.resetOpacity()
+                    g2.color = ColorPalette.background
+                    g2.fillRoundRect(220, 400, 75, 35, 10, 10)
+
+                    g.color = ColorPalette.foreground
+                    g.font = CustomFont.regular?.deriveFont(24f)
+                    g.drawString("Static", 225, 426)
+
+                    g2.resetOpacity()
+                    g2.color = ColorPalette.foreground
+                    g2.fillRoundRect(220 + 100, 400, 170, 35, 10, 10)
+
+                    g.color = ColorPalette.background
+                    g.font = CustomFont.regular?.deriveFont(24f)
+                    g.drawString("Color Gradient", 225 + 100, 426)
+
+                    g2.resetOpacity()
+                    g2.color = ColorPalette.foreground
+                    g2.fillRoundRect(320 + 190, 400, 170, 35, 10, 10)
+
+                    g.color = ColorPalette.background
+                    g.font = CustomFont.regular?.deriveFont(24f)
+                    g.drawString("Rainbow Wave", 325 + 190, 426)
+                }
+                EffectsEnum.COLOR_GRADIENT -> {
+                    g2.resetOpacity()
+                    g2.color = ColorPalette.foreground
+                    g2.fillRoundRect(220, 400, 75, 35, 10, 10)
+
+                    g.color = ColorPalette.background
+                    g.font = CustomFont.regular?.deriveFont(24f)
+                    g.drawString("Static", 225, 426)
+
+                    g2.resetOpacity()
+                    g2.color = ColorPalette.background
+                    g2.fillRoundRect(220 + 100, 400, 170, 35, 10, 10)
+
+                    g.color = ColorPalette.foreground
+                    g.font = CustomFont.regular?.deriveFont(24f)
+                    g.drawString("Color Gradient", 225 + 100, 426)
+
+                    g2.resetOpacity()
+                    g2.color = ColorPalette.foreground
+                    g2.fillRoundRect(320 + 190, 400, 170, 35, 10, 10)
+
+                    g.color = ColorPalette.background
+                    g.font = CustomFont.regular?.deriveFont(24f)
+                    g.drawString("Rainbow Wave", 325 + 190, 426)
+                }
+                EffectsEnum.RAINBOW_WAVE -> {
+                    g2.resetOpacity()
+                    g2.color = ColorPalette.foreground
+                    g2.fillRoundRect(220, 400, 75, 35, 10, 10)
+
+                    g.color = ColorPalette.background
+                    g.font = CustomFont.regular?.deriveFont(24f)
+                    g.drawString("Static", 225, 426)
+
+                    g2.resetOpacity()
+                    g2.color = ColorPalette.foreground
+                    g2.fillRoundRect(220 + 100, 400, 170, 35, 10, 10)
+
+                    g.color = ColorPalette.background
+                    g.font = CustomFont.regular?.deriveFont(24f)
+                    g.drawString("Color Gradient", 225 + 100, 426)
+
+                    g2.resetOpacity()
+                    g2.color = ColorPalette.background
+                    g2.fillRoundRect(320 + 190, 400, 170, 35, 10, 10)
+
+                    g.color = ColorPalette.foreground
+                    g.font = CustomFont.regular?.deriveFont(24f)
+                    g.drawString("Rainbow Wave", 325 + 190, 426)
+                }
+            }
+
+
+        }
+    }
+
+    override fun mouseClicked(x: Int, y: Int) {
+        if (needSecondaryColor) {
+            for (widget in widgets) {
+                widget.mouseClicked(x, y)
+            }
+        }
+
+        if (x in 979..1039 && y in 239..284) {
+            exec(redSlider.value, greenSlider.value, blueSlider.value)
+            open = false
+        } else if (x in 969..1044 && y in 379..484) {
+            open = false
+        } else if (x in 220..275 && y in 370..435) {
+            effect = EffectsEnum.STATIC
+            needSecondaryColor = false
+        } else if (x in 320..430 && y in 370..435) {
+            effect = EffectsEnum.COLOR_GRADIENT
+            needSecondaryColor = true
+        } else if (x in 510..680 && y in 370..435) {
+            effect = EffectsEnum.RAINBOW_WAVE
+            needSecondaryColor = false
+        }
+
+    }
+
+    override fun mouseMoved(x: Int, y: Int) {
+        if (needSecondaryColor) {
+            for (widget in widgets) {
+                widget.mouseMoved(x, y)
+            }
+        }
+
+        hoveredApply = (x in 979..1039 && y in 239..284)
+        hoveredCancel = (x in 969..1044 && y in 379..484)
+
+    }
+
+    override fun dragMouse(x: Int, y: Int) {
+        if (needSecondaryColor) {
+            for (widget in widgets) {
+                widget.dragMouse(x, y)
+            }
+        }
+    }
+
+    override fun mousePressed(x: Int, y: Int) {
+        for (widget in widgets) {
+            widget.mousePressed(x, y)
+        }
+    }
+
+}
--- src/main/java/userinterface/screens/DeviceZoneScreen.kt
+++ src/main/java/userinterface/screens/DeviceZoneScreen.kt
@@ -8,10 +8,13 @@ import userinterface.CustomFont
 import userinterface.Screen
 import userinterface.WindowHandler
 import userinterface.popups.ColorPickerPopup
+import userinterface.popups.EffectPickerPopup
 import java.awt.Color
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.image.ImageObserver
+import java.io.File
+import javax.imageio.ImageIO
 
 class DeviceZoneScreen(val deviceName: String, val deviceIndex: Int) : Screen() {
 
@@ -32,9 +35,9 @@ class DeviceZoneScreen(val deviceName: String, val deviceIndex: Int) : Screen() 
 
             zoneColorButton[zoneIndex] = ZoneConfigurationButton(
                 zoneName, zoneIndex, if (zoneIndex <= 8) {
-                    566
+                    566 - 125
                 } else {
-                    996
+                    996 - 125
                 }, drawZoneY - 19
             )
 
@@ -75,22 +78,49 @@ class DeviceZoneScreen(val deviceName: String, val deviceIndex: Int) : Screen() 
 
             g.fillRoundRect(
                 if (zoneIndex <= 8) {
-                    557
+                    557 - 125
                 } else {
-                    987
+                    987 - 125
                 }, drawZoneY - 27, 35, 35, 10, 10
             )
 
             g.color = Color.decode(zoneColorButton[zoneIndex]!!.colorHex)
             g.fillRoundRect(
                 if (zoneIndex <= 8) {
-                    560
+                    560 - 125
                 } else {
-                    990
+                    990 - 125
                 }, drawZoneY - 24, 29, 29, 10, 10
             )
 
             g.color = ColorPalette.foreground
+
+            g.fillRoundRect(
+                if (zoneIndex <= 8) {
+                    610 - 125
+                } else {
+                    1040 - 125
+                }, drawZoneY - 26, 100, 35, 10, 10
+            )
+
+            g.font = CustomFont.regular?.deriveFont(22f)
+            g.color = ColorPalette.background
+            g.drawString(
+                "Effect", if (zoneIndex <= 8) {
+                    615 - 125
+                } else {
+                    1045 - 125
+                }, drawZoneY
+            )
+
+            g.drawImage(
+                ImageIO.read(File("files\\images\\devices\\configure.png")), if (zoneIndex <= 8) {
+                    615 - 60
+                } else {
+                    1045 - 60
+                }, drawZoneY - 20,
+                observer
+            )
 
             if (zoneIndex == 8) {
                 drawZoneY = 175
@@ -123,6 +153,12 @@ class DeviceZoneScreen(val deviceName: String, val deviceIndex: Int) : Screen() 
 
                         WindowHandler.popup = null
                     }, color.red, color.green, color.blue
+                )
+            } else if (x in (button.x + 53) until (button.x + 153) && y in (button.y - 50) until (button.y)) {
+                WindowHandler.popup = EffectPickerPopup(
+                    { red: Int, green: Int, blue: Int ->
+                        println("Selected secondary color is ${String.format("#%02x%02x%02x", red, green, blue)}")
+                    }, 0, 0, 0
                 )
             }
         }
