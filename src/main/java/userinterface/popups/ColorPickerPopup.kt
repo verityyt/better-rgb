a7958fcfa8239d2fa4673aaa713cf14e1cce48fc
Verity <joshua-schmidt@online.de>
2021-04-06 12:16:24 +0200

    :sparkles: Add cancel button to ColorPicker

--- src/main/java/userinterface/popups/ColorPickerPopup.kt
+++ src/main/java/userinterface/popups/ColorPickerPopup.kt
@@ -17,13 +17,14 @@ class ColorPickerPopup(
     private val oldBlue: Int = 0
 ) : Popup() {
 
-    private var redSlider = ColorSliderWidget(415, 275, Color.red, "R", oldRed)
-    private var greenSlider = ColorSliderWidget(415, 315, Color.green, "G", oldGreen)
-    private var blueSlider = ColorSliderWidget(415, 355, Color.blue, "B", oldBlue)
+    private var redSlider = ColorSliderWidget(385, 275, Color.red, "R", oldRed)
+    private var greenSlider = ColorSliderWidget(385, 315, Color.green, "G", oldGreen)
+    private var blueSlider = ColorSliderWidget(385, 355, Color.blue, "B", oldBlue)
 
     private var widgets = listOf(redSlider, greenSlider, blueSlider)
 
-    private var hoveredClose = false
+    private var hoveredApply = false
+    private var hoveredCancel = false
     private var open = true
 
     override fun paint(g: Graphics, g2: Graphics2D, observer: ImageObserver) {
@@ -39,15 +40,15 @@ class ColorPickerPopup(
             /* Preview */
 
             g.color = ColorPalette.foreground
-            g.fillRoundRect(250, 270, 110, 110, 25, 25)
+            g.fillRoundRect(220, 270, 110, 110, 25, 25)
 
             g.color = Color(redSlider.value, greenSlider.value, blueSlider.value)
-            g.fillRoundRect(255, 275, 100, 100, 25, 25)
+            g.fillRoundRect(225, 275, 100, 100, 25, 25)
 
             /* Done button */
             g2.color = ColorPalette.foreground
             g2.setOpacity(
-                if (hoveredClose) {
+                if (hoveredApply) {
                     1f
                 } else {
                     0.6f
@@ -56,6 +57,17 @@ class ColorPickerPopup(
 
             g2.font = CustomFont.light?.deriveFont(24f)
             g2.drawString("Apply", 980, 280)
+
+            g2.color = ColorPalette.foreground
+            g2.setOpacity(
+                if (hoveredCancel) {
+                    1f
+                } else {
+                    0.6f
+                }
+            )
+            g2.drawString("Cancel", 970, 390)
+
         }
     }
 
@@ -67,6 +79,8 @@ class ColorPickerPopup(
         if (x in 979..1039 && y in 239..284) {
             exec(redSlider.value, greenSlider.value, blueSlider.value)
             open = false
+        }else if (x in 969..1044 && y in 349..394) {
+            open = false
         }
 
     }
@@ -76,7 +90,8 @@ class ColorPickerPopup(
             widget.mouseMoved(x, y)
         }
 
-        hoveredClose = (x in 979..1039 && y in 239..284)
+        hoveredApply = (x in 979..1039 && y in 239..284)
+        hoveredCancel = (x in 969..1044 && y in 349..394)
 
     }
 
