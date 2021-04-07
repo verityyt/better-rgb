fd97b7a5a140bdfb3e413413db9fa19fead07595
Verity <joshua-schmidt@online.de>
2021-04-07 14:41:31 +0200

    :memo: Add KDocs to kotlin.utils.*

--- src/main/kotlin/utils/Extentions.kt
+++ src/main/kotlin/utils/Extentions.kt
@@ -3,10 +3,16 @@ package utils
 import java.awt.AlphaComposite
 import java.awt.Graphics2D
 
+/**
+ * Extention function for [Graphics2D] to set opacity/[AlphaComposite] to given float
+ */
 fun Graphics2D.setOpacity(opacity: Float) {
     this.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity)
 }
 
+/**
+ * Extention function for [Graphics2D] to reset opacity/[AlphaComposite] to 1.0f
+ */
 fun Graphics2D.resetOpacity() {
     this.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f)
 }
