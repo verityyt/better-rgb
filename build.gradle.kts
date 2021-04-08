9c8456fe9a39f60a2129726919fd7deecfad6d4f
Verity <joshua-schmidt@online.de>
2021-04-08 16:11:11 +0200

    :wrench: Add 'export' gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,42 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    jar {
+        archiveName = "BetterRGB.jar"
+
+        from(sourceSets.main.get().output)
+        dependsOn(configurations.runtimeClasspath)
+
+        from({
+            configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+        })
+
+        manifest {
+            attributes["Main-Class"] = "BetterRGB"
+        }
+
+        exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+    }
+}
+
+tasks.register("export") {
+    group = "build"
+    description = "Exports the project as jar file"
+
+    dependsOn("jar")
+}
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
