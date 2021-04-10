378eb1b216a7034a493ef6cd2a38bd25dc8eae3c
Verity <joshua-schmidt@online.de>
2021-04-10 21:42:23 +0200

    :sparkles: Implement 'needSpeed' to EffectsEnum

--- src/main/kotlin/openrgb/EffectsEnum.kt
+++ src/main/kotlin/openrgb/EffectsEnum.kt
@@ -1,11 +1,16 @@
 package openrgb
 
-enum class EffectsEnum(val displayName: String, val needPrimary: Boolean, val needSecondary: Boolean) {
+enum class EffectsEnum(
+    val displayName: String,
+    val needPrimary: Boolean,
+    val needSecondary: Boolean,
+    val needSpeed: Boolean
+) {
 
-    STATIC("Static",true, false),
-    COLOR_GRADIENT("Color Gradient",true, true),
-    RAINBOW_WAVE("Rainbow Wave",false, false),
-    BREATHING("Breathing", true, false),
-    FLASHING("Flashing",true, true)
+    STATIC("Static", true, false, false),
+    COLOR_GRADIENT("Color Gradient", true, true, true),
+    RAINBOW_WAVE("Rainbow Wave", false, false, true),
+    BREATHING("Breathing", true, false, true),
+    FLASHING("Flashing", true, true, true)
 
 }
